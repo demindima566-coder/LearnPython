28487f092bc7e9ad847aa1f81f4a2f986c2ca0c6
demindima566-coder <demindima566@gmail.com>
2026-05-20 14:52:07 +0200

    Выполнил дз sandwich_v2

--- homework/2026_02_20/2_sandwitch_v2/main.py
+++ homework/2026_02_20/2_sandwitch_v2/main.py
@@ -1,15 +1,32 @@
 def bread(func):
-    pass
+    def wrapper():
+        print("Bread")
+        func()
+        print("Bread")
+    return wrapper
 
 def salat(func):
-    pass
+    def wrapper():
+        print("Salat")
+        func()
+    return wrapper
 
 def tomato(func):
-    pass
+    def wrapper():
+        print("Tomato")
+        func()
+    return wrapper
 
 def meat(func):
-    pass
+    def wrapper():
+        print("Meat")
+        func()
+    return wrapper
 
+@bread
+@salat
+@tomato
+@meat
 def make_sandwich():
     return ''
 
