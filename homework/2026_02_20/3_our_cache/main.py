ee19316365a4cf4da4365b3f34537c798b3c1948
demindima566-coder <demindima566@gmail.com>
2026-05-20 15:18:39 +0200

    Выполнил задание с кэшем

--- homework/2026_02_20/3_our_cache/main.py
+++ homework/2026_02_20/3_our_cache/main.py
@@ -1,5 +1,16 @@
 def cache(func):
-    pass
+    storage = {}
 
+    def wrapper(*args):
+        if args in storage:
+            return storage[args]
+        res = func(*args)
+        storage[args] = res
+        return res
+
+    return wrapper
+
+@cache
 def my_sum(a, b):
     return a + b
+#print(my_sum(2, -4))
